24048c30a73becf7898279d46bfbf5a8bc824d1b
dittricn2 <Nicholas_Dittrich@kenan-flagler.unc.edu>
2025-02-25 10:19:11 -0500

    Update identity.ts
    Changed AI Tone

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `Nicholas Dittrich`;
 export const OWNER_DESCRIPTION: string = `MBA Student at Kenan Flagler`;
 
 export const AI_NAME: string = `Jim Madden`;
-export const AI_TONE: string = `[AI TONE]`;
+export const AI_TONE: string = `Friendly`;
 export const AI_ROLE: string = `[AI ROLE]`;
