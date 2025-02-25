9d9279a37d7b1a4cd4937ece5615469219b933d8
dittricn2 <Nicholas_Dittrich@kenan-flagler.unc.edu>
2025-02-25 09:50:39 -0500

    Update identity.ts
    Renamed Bot

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Nicholas Dittrich`;
 export const OWNER_DESCRIPTION: string = `MBA Student at Kenan Flagler`;
 
-export const AI_NAME: string = `[AI NAME]`;
+export const AI_NAME: string = `Jim Madden`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
