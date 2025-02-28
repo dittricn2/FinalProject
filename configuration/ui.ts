1d95cf574d7a6786f205484231aee6b46297e743
dittricn2 <Nicholas_Dittrich@kenan-flagler.unc.edu>
2025-02-28 17:41:10 -0500

    Update ui.ts
    Edited Page Description

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -5,6 +5,6 @@ export const MESSAGE_PLACEHOLDER: string = `Placeholder Message`;
 export const FOOTER_MESSAGE: string = `2025 UNC Kenan Flagler`;
 export const CLEAR_BUTTON_TEXT: string = `Clear Conversation`;
 export const PAGE_TITLE: string = `Learn All About the NFL Football With This Helpful Chatbot`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s NFL Football AI assistant.`;
+export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s NFL Football AI assistant. Feel free to ask me questions to learn more about the NFL or fantasy football!`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
